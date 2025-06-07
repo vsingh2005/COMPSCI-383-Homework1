c0fa529454a115a5e58fd4957875fd64bf0a9dee
Vansh Singh <singhvansh@outlook.com>
2025-06-07 14:01:10 -0500

    updated solver.py to reduce duplicate frontier nodes

--- solver.py
+++ solver.py
@@ -31,7 +31,6 @@ def solve_puzzle(start_state, flavor):
             during the search.
         'expanded_count' - the number of unique states removed from the frontier and expanded 
             (successors generated)
-
     """
     if flavor.find('-') > -1:
         strat, heur = flavor.split('-')
@@ -47,7 +46,7 @@ def solve_puzzle(start_state, flavor):
     elif strat == 'astar':
         return AStarSolver(heuristic=heur).solve(start_state)
     else:
-        raise ValueError("Unknown search flavor '{}'".format(flavor))
+        raise ValueError(f"Unknown search flavor '{flavor}'")
 
 
 class BreadthFirstSolver:
@@ -82,43 +81,41 @@ class BreadthFirstSolver:
             node = self.frontier.pop()
             self.explored.add(node)
             self.expanded_count += 1
-            for move, succ in node.successors().items():
-                if succ not in self.explored and succ not in self.frontier:
+            succs = node.successors()
+            for move, succ in succs.items():
+                if (succ not in self.explored) and (succ not in self.frontier):
                     self.parents[succ] = node
-
                     # BFS checks for goal state _before_ adding to frontier
                     if succ == self.goal:
                         return self.get_results_dict(succ)
                     self.frontier.add(succ)
                     self.frontier_count += 1
 
+        # if we get here, the search failed
         return self.get_results_dict(None)
 
     def get_results_dict(self, state):
-        results = {
-            'frontier_count': self.frontier_count,
-            'expanded_count': self.expanded_count
-        }
+        results = {}
+        results['frontier_count'] = self.frontier_count
+        results['expanded_count'] = self.expanded_count
         if state:
             path = self.get_path(state)
-            moves = ['start'] + [path[i - 1].get_move(path[i]) for i in range(1, len(path))]
+            moves = ['start'] + [ path[i-1].get_move(path[i]) for i in range(1, len(path)) ]
             results['path'] = list(zip(moves, path))
             results['path_cost'] = self.get_cost(path)
         return results
 
     def get_path(self, state):
         """Return the solution path from the start state of the search to a target.
-        
         Results are obtained by retracing the path backwards through the parent tree to the start
         state for the serach at the root.
-        
+
         Args:
             state (EightPuzzleBoard): target state in the search tree
-        
+
         Returns:
             A list of EightPuzzleBoard objects representing the path from the start state to the
             target state
-
         """
         path = []
         while state is not None:
@@ -129,66 +126,30 @@ class BreadthFirstSolver:
 
     def get_cost(self, path):
         """Calculate the path cost from start state to a target state.
-        
         Transition costs between states are equal to the square of the number on the tile that 
         was moved. 
 
         Args:
             state (EightPuzzleBoard): target state in the search tree
-        
+
         Returns:
             Integer indicating the cost of the solution path
-
         """
         cost = 0
         for i in range(1, len(path)):
-            x, y = path[i - 1].find(None)
+            x, y = path[i-1].find(None)
             tile = path[i].get_tile(x, y)
             cost += int(tile) ** 2
         return cost
 
-def print_table(flav__results, include_path=False):
-    """Print out a comparison of search strategy results.
-
-    Args:
-        flav__results (dictionary): a dictionary mapping search flavor tags result statistics. See
-            solve_puzzle() for detail.
-        include_path (bool): indicates whether to include the actual solution paths in the table
-
-    """
-    result_tups = sorted(flav__results.items())
-    c = len(result_tups)
-    na = "{:>12}".format("n/a")
-    rows = [  # abandon all hope ye who try to modify the table formatting code...
-        "flavor  " + "".join([ "{:>12}".format(tag) for tag, _ in result_tups]),
-        "--------" + ("  " + "-"*10)*c,
-        "length  " + "".join([ "{:>12}".format(len(res['path'])) if 'path' in res else na 
-                                for _, res in result_tups ]),
-        "cost    " + "".join([ "{:>12,}".format(res['path_cost']) if 'path_cost' in res else na 
-                                for _, res in result_tups ]),
-        "frontier" + ("{:>12,}" * c).format(*[res['frontier_count'] for _, res in result_tups]),
-        "expanded" + ("{:>12,}" * c).format(*[res['expanded_count'] for _, res in result_tups])
-    ]
-    if include_path:
-        rows.append("path")
-        longest_path = max([ len(res['path']) for _, res in result_tups if 'path' in res ] + [0])
-        print("longest", longest_path)
-        for i in range(longest_path):
-            row = "        "
-            for _, res in result_tups:
-                if len(res.get('path', [])) > i:
-                    move, state = res['path'][i]
-                    row += " " + move[0] + " " + str(state)
-                else:
-                    row += " "*12
-            rows.append(row)
-    print("\n" + "\n".join(rows), "\n")
 
 class SearchSolver:
+    """Base class for Uniform-cost, Greedy, and A* solvers"""
+
     def __init__(self, heuristic=None):
         self.goal = GOAL_STATE
         self.parents = {}
-        self.g_scores = {}
+        self.g_scores = {}  # tracks g(n)
         self.heuristic = heuristic
         self.frontier = pdqpq.PriorityQueue()
         self.explored = set()
@@ -196,6 +157,7 @@ class SearchSolver:
         self.expanded_count = 0
 
     def solve(self, start_state):
+        """Generic graph-search algorithm parametrized by f(n) = g(n)+h(n)."""
         self.parents[start_state] = None
         self.g_scores[start_state] = 0
         self.frontier.add(start_state, self.f(start_state))
@@ -203,7 +165,6 @@ class SearchSolver:
 
         while not self.frontier.is_empty():
             node = self.frontier.pop()
-
             if node == self.goal:
                 return self.get_results_dict(node)
 
@@ -211,28 +172,42 @@ class SearchSolver:
             self.expanded_count += 1
 
             for move, succ in node.successors().items():
-                cost = int(node.get_tile(*succ.find(None))) ** 2
-                new_g = self.g_scores[node] + cost
-                if succ not in self.g_scores or new_g < self.g_scores[succ]:
+                step_cost = int(node.get_tile(*succ.find(None))) ** 2
+                new_g = self.g_scores[node] + step_cost
+                if (succ not in self.g_scores) or (new_g < self.g_scores[succ]):
                     self.parents[succ] = node
                     self.g_scores[succ] = new_g
-                    if succ not in self.explored:
+                    in_front = succ in self.frontier
+                    in_explored = succ in self.explored
+                    if (not in_explored) and (not in_front):
                         self.frontier.add(succ, self.f(succ))
                         self.frontier_count += 1
+                    elif in_front:
+                        # priority update
+                        self.frontier.add(succ, self.f(succ))
 
+        # no solution found
         return self.get_results_dict(None)
 
     def h(self, state):
+        """Heuristic dispatch: h1, h2, or h3."""
         if self.heuristic == 'h1':
-            return sum(1 for i in range(1, 9) if state.find(str(i)) != GOAL_STATE.find(str(i)))
-        elif self.heuristic == 'h2':
-            return sum(abs(state.find(str(i))[0] - GOAL_STATE.find(str(i))[0]) +
-                       abs(state.find(str(i))[1] - GOAL_STATE.find(str(i))[1])
-                       for i in range(1, 9))
-        elif self.heuristic == 'h3':
-            return sum((abs(state.find(str(i))[0] - GOAL_STATE.find(str(i))[0]) +
-                        abs(state.find(str(i))[1] - GOAL_STATE.find(str(i))[1])) * int(i) ** 2
-                       for i in range(1, 9))
+            # number of misplaced tiles
+            return sum(1 for i in range(1,9) if state.find(str(i)) != GOAL_STATE.find(str(i)))
+        if self.heuristic == 'h2':
+            # Manhattan distance
+            return sum(
+                abs(state.find(str(i))[0] - GOAL_STATE.find(str(i))[0]) +
+                abs(state.find(str(i))[1] - GOAL_STATE.find(str(i))[1])
+                for i in range(1,9)
+            )
+        if self.heuristic == 'h3':
+            # weighted Manhattan (cost squared)
+            return sum(
+                (abs(state.find(str(i))[0] - GOAL_STATE.find(str(i))[0]) +
+                 abs(state.find(str(i))[1] - GOAL_STATE.find(str(i))[1])) * int(i)**2
+                for i in range(1,9)
+            )
         return 0
 
     def f(self, state):
@@ -245,7 +220,7 @@ class SearchSolver:
         }
         if state:
             path = self.get_path(state)
-            moves = ['start'] + [path[i - 1].get_move(path[i]) for i in range(1, len(path))]
+            moves = ['start'] + [path[i-1].get_move(path[i]) for i in range(1,len(path))]
             results['path'] = list(zip(moves, path))
             results['path_cost'] = self.g_scores[state]
         return results
@@ -274,6 +249,27 @@ class AStarSolver(SearchSolver):
         return self.g_scores[state] + self.h(state)
 
 
+def print_table(results, include_path=False):
+    tags = sorted(results.keys())
+    headers = ['flavor'] + tags
+    metrics = ['length','cost','frontier','expanded']
+
+    # header row
+    print(''.join(f"{h:>12}" for h in headers))
+    print('-'*12*len(headers))
+    # metric rows
+    for m in metrics:
+        row = [m]
+        for tag in tags:
+            res = results[tag]
+            if m=='length': val = len(res['path']) if 'path' in res else 'n/a'
+            if m=='cost':   val = res.get('path_cost','n/a')
+            if m=='frontier': val = res['frontier_count']
+            if m=='expanded': val = res['expanded_count']
+            row.append(val)
+        print(''.join(f"{str(c):>12}" for c in row))
+
+
 def get_test_puzzles():
     test1 = puzz.EightPuzzleBoard("125340678")  # 3-5 moves
     test2 = puzz.EightPuzzleBoard("724506831")  # 10-15 moves
@@ -282,8 +278,6 @@ def get_test_puzzles():
 
 
 if __name__ == '__main__':
-
-    # parse the command line args
     start = puzz.EightPuzzleBoard(sys.argv[1])
     if sys.argv[2] == 'all':
         flavors = ['bfs', 'ucost', 'greedy-h1', 'greedy-h2', 
@@ -294,7 +288,7 @@ if __name__ == '__main__':
     # run the search(es)
     results = {}
     for flav in flavors:
-        print("solving puzzle {} with {}".format(start, flav))
+        print(f"solving puzzle {start} with {flav}")
         results[flav] = solve_puzzle(start, flav)
 
-    print_table(results, include_path=False)  # change to True to see the paths!
+    print_table(results, include_path=False)
